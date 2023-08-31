4052135550962d14f4442d6d89748b5622b636a5
nataliarcr <nravylla@gmail.com>
2023-08-31 10:42:40 -0300

    encapsulamento

--- poo/bin/poo.dart
+++ poo/bin/poo.dart
@@ -1,1 +1,9 @@
-void main(List<String> arguments) {}
+import 'package:poo/classes/pessoa.dart';
+
+void main(List<String> arguments) {
+  var p1 = new Pessoa();
+  p1.setName('Nati');
+  p1.setEndereco('Rua Iraci');
+  print(p1.nome);
+  print(p1.getEndereco());
+}
--- poo/lib/classes/pessoa.dart
+++ poo/lib/classes/pessoa.dart
@@ -1,1 +1,16 @@
-class Pessoa {}
+class Pessoa {
+  String? nome = '';
+  String _endereco = '';
+
+  void setName(String nome) {
+    this.nome = nome;
+  }
+
+  void setEndereco(String endereco) {
+    _endereco = endereco;
+  }
+
+  String getEndereco() {
+    return _endereco;
+  }
+}
